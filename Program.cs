a48e79304b1f43e956ffbe2c7580655c4691155c
L Pollard (Ada Developers Academy) <v-lpollard@microsoft.com>
2023-11-07 14:09:53 -0600

    creates guess the number app

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace NumberGuessingGame
+  {
+    class Program
+    {
+      static void Main(string[] args)
+      {
+          Random r = new Random();
+
+          int winNum = r.Next(0, 100);
+
+          bool win = false;
+
+          do 
+
+          {
+            Console.Write("Guess a number between 0-100");
+            string s = Console.ReadLine();
+            int i = int.Parse(s);
+
+            if (i > winNum)
+            {
+              Console.WriteLine("too high, guess again");
+            }
+            else if (i < winNum)
+            {
+              Console.WriteLine("too low, guess again");
+            }
+            else if (i == winNum)
+            {
+              Console.WriteLine("Correct Guess, you win");
+              win = true;
+            }
+            Console.WriteLine();
+
+          } while (win == false);
+          Console.WriteLine("Thanks for playing");
+          Console.Write("Press any key to finish");
+          Console.ReadKey(true);
+      }
+    }
+  }
